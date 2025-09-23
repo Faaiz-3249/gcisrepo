fc925dcd7d951f50d2f0e44f6367667acf78b0e9
Faaiz-3249 <mh3249@g.rit.edu>
2025-09-23 10:50:03 +0400

    hi

---
+++ test.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter a Number: "))
+
+
+def even_or_odd():
+    if num1 % 2 == 0:
+        print("Number is even")
+    else:
+        print("Numebr is odd")
+
+
+def sqr_cube():
+    print(num1**2)
+    print(num1**3)
+
+
+even_or_odd()
+sqr_cube()
